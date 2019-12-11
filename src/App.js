2ed2198c2773e3f8809c93d298edd675e5706e8b
rixhkai <rijallconnie@gmail.com>
2019-12-11 14:04:53 +0700

    4.Category

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import { Typography, Button } from '@material-ui/core';
+import Container from '@material-ui/core/Container';
+import SearchAppBar from './NavBar';
+import ScrollableTabsButtonForce from './Tab';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <div >
+      <header >
+      
+        </header>
+        <SearchAppBar/>
+      
+     
+    </div>
+    
+    <ScrollableTabsButtonForce position="sticky"/>
+    
     </div>
   );
 }
